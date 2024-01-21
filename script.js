487a5f7cedea8413c4fd39ee8876c8b06cef01e2
Jimmy <mahmoudgamalawad10@gmail.com>
2024-01-21 04:58:16 +0200

    home page

---
+++ script.js
@@ -0,0 +1,11 @@
+let barsIcon = document.querySelector(".icon");
+let closeIcon = document.querySelector(".fa-xmark");
+let nav = document.querySelector("nav");
+
+barsIcon.addEventListener("click", () => {
+  nav.classList.toggle("show-nav");
+});
+
+closeIcon.addEventListener("click", () => {
+  nav.classList.remove("show-nav");
+});
